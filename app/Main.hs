fc52ea51e7ae72fc1917f8e79c5bc817e9636c8b
Bert Van hauwermeiren <vanhauwermeiren.bert@gmail.com>
2022-07-26 16:48:16 +0200

    shuffle and sort a list

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import Lib ()
+import System.Random.Shuffle ( shuffle' )
+import System.Random ( mkStdGen, StdGen )
+import Data.List ( sort )
+
+
+list :: [Int]
+list = [1..20]
+
+gen :: StdGen
+gen = mkStdGen 15
+
+shuffledList :: [Int]
+shuffledList = shuffle' list (length list) gen
+
+sortedList :: [Int]
+sortedList = sort shuffledList
 
 main :: IO ()
-main = putStrLn "<3"
+main = do
+    print list
+    print shuffledList
+    print sortedList

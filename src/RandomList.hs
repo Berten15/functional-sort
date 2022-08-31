109bb2d62b1eab43dea4dba23be7ee85d0b40994
Bert Van hauwermeiren <vanhauwermeiren.bert@gmail.com>
2022-08-31 17:18:57 +0200

    Updated getFewUniqueList

--- src/RandomList.hs
+++ src/RandomList.hs
@@ -16,6 +16,7 @@ getRandomList listLength seed = shuffle' [1..listLength] listLength (mkStdGen se
 getNearlySortedList :: Int -> Int -> [Int]
 getNearlySortedList listLength = randomiseCouple [1..listLength]
 
+-- Replaces one element every 15 elements in list with 4, first replacement depends on seed
 randomiseCouple :: [Int] -> Int -> [Int]
 randomiseCouple [] seed = []
 randomiseCouple list 0 = 4 : randomiseCouple (tail list) 15
@@ -25,9 +26,9 @@ randomiseCouple list seed = head list : randomiseCouple (tail list) (seed-1)
 getReverseSortedList :: Int -> Int -> [Int]
 getReverseSortedList listLength seed = [listLength..1]
 
-
--- listLength should be multiple of 50 
+-- Returns list with all elements in [1..10]
+-- listLength should be multiple of 10
 getFewUniqueList :: Int -> Int -> [Int]
 getFewUniqueList listLength seed = do
-    let list = [50*x | x <- [1..(listLength `div` 50)]] >>= replicate 50 
+    let list = concatMap (replicate $ listLength `div` 10) [1..10]
     shuffle' list listLength (mkStdGen seed)
